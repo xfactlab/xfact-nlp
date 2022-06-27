1819f1bfc595e78dd7db175b805082ac91de2213
James Thorne <james@jamesthorne.co.uk>
2022-06-27 15:23:22 +0900

    update scheduler

--- scripts/vessl-create.sh
+++ scripts/vessl-create.sh
@@ -4,10 +4,10 @@ vessl experiment create \
   --image-url "j6mes/dev-env:build-1" \
   --cluster "cluster-2080" \
   --resource "gpu-1" \
-  --command "bash scripts/deardr/pretrain.sh 1 \$reader \$learning_rate \$batch_size \$steps \$eval_freq" \
+  --command "bash -x scripts/deardr/pretrain.sh 1 \$reader \$learning_rate \$batch_size \$steps \$eval_freq \$lr_scheduler_type" \
   --working-dir /root/deardr --root-volume-size "100Gi" --output-dir "/output/" \
   --dataset "/deardr/:kaist-jtlab/deardr" \
   --git-ref "/root/deardr:github/j6mes/deardr/HEAD" \
   --dataset "/fever/:kaist-jtlab/fever" \
-  -h reader=pretrain_hl -h learning_rate=1e-5 -h batch_size=8 -h steps=1 -h eval_freq=400
+  -h reader=pretrain_hl -h learning_rate=1e-5 -h batch_size=8 -h steps=1 -h eval_freq=400 -h lr_scheduler_type=linear
 
