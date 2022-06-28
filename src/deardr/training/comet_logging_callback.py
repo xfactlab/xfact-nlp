f89000b456c2d23c4a229c34fcf394a36f167fe2
James Thorne <james@jamesthorne.co.uk>
2022-06-28 16:01:51 +0900

    output debug line

--- src/deardr/training/comet_logging_callback.py
+++ src/deardr/training/comet_logging_callback.py
@@ -7,6 +7,7 @@ class CometTrainingCallback(TrainerCallback):
 
     def on_log(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         if self.expt is not None and args.local_rank <= 0:
+            print("Saving logs")
             self.expt.log(step=state.global_step, payload={"train/" + key: value for key, value in state.log_history[-1].items()})
 
             # self.expt.log_metrics({"train/" + key: value for key, value in state.log_history[-1].items()},
