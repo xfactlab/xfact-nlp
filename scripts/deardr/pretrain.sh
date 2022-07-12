4b92fdd53aae96200dda72e53aed06ecf9eb62b6
euiyulsong <eysong@uw.edu>
2022-07-12 18:52:01 +0900

    feat: roll-back hp

--- scripts/deardr/pretrain.sh
+++ scripts/deardr/pretrain.sh
@@ -30,7 +30,7 @@ python -m torch.distributed.launch --nproc_per_node=$NUM_GPUS \
   --logging_steps $log_freq \
   --save_steps $eval_freq \
   --eval_steps $eval_freq \
-  --num_train_epochs 1 \
+  --num_train_epochs 8 \
   --save_total_limit 3 \
   --max_eval_samples 1000 \
   --load_best_model_at_end \
