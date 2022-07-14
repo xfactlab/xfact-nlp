7afc8225156a824de75ab46f67c728ac93134d44
EuiYul Song <euiyulsong@EuiYului-MacBookAir.local>
2022-07-14 16:06:15 +0900

    feat: add

--- scripts/vessl-create.sh
+++ scripts/vessl-create.sh
@@ -18,7 +18,7 @@ vessl experiment create \
   --git-ref "/root/deardr:github/j6mes-lab/deardr/${GIT_REF}" \
   --dataset "/data/:kaist-jtlab/deardr-dataset" \
   --dataset "/cache/:kaist-jtlab/cache" \
-  -h reader=pretrain_hl -h learning_rate=1e-5 -h batch_size=8 -h steps=1 -h eval_freq=400 -h lr_scheduler_type=linear \
+  -h reader=pretrain_pthl -h learning_rate=1e-5 -h batch_size=8 -h steps=1 -h eval_freq=400 -h lr_scheduler_type=linear \
   -h val_reader=fever -h train_db=wiki-pretraining -h val_db=fever -h train_file=shuf_10k.jsonl -h val_file=shared_task_dev.jsonl \
   -h model_name=t5-base \
   -h DATA_ROOT=/data -h TRANSFORMERS_CACHE=/cache/transformers -h XDG_CACHE_HOME=/cache/pytorch
