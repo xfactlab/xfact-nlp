e650bb20a07f6a5b5bb77f12f214c9c4789a6326
euiyulsong <eysong@uw.edu>
2022-07-15 19:26:47 +0900

    feat: experiment with different hyperparameter

--- scripts/hp_tune.sh
+++ scripts/hp_tune.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-start=0.00005
-end=0.000005
-factor=-0.000005
-gpu=4
-for i in 0.00005 0.00001 0.000005 0.000001 # 0.0000005 0.0000001 0.00000005 0.00000001
+gpu=2
+for i in 0.00005 0.00001 0.000005 # 0.000001 0.0000005 0.0000001 0.00000005 0.00000001
 do
   for j in "pretrain_hl" "pretrain_pthl" "pretrain_pt"
   do
-    for k in "constant" "linear" #"cosine_with_restarts" "polynomial" "constant" "constant_with_warmup"
+    for k in "cosine_with_restarts" "polynomial" "constant" "constant_with_warmup" # "constant" "linear" #
     do
       bash vessl-create.sh $gpu $i $j $k
     done
