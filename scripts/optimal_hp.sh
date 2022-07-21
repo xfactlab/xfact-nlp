693cf648a78d7e94a97003706b88be25653cbfe0
euiyulsong <eysong@uw.edu>
2022-07-21 15:38:29 +0900

    fix: rollback to the original

--- scripts/optimal_hp.sh
+++ scripts/optimal_hp.sh
@@ -1,3 +1,4 @@
 bash vessl-create.sh 8 0.00001 pretrain_pt polynomial
 bash vessl-create.sh 8 0.0005 pretrain_pthl constant
+bash vessl-create.sh 8 0.00005 pretrain_pt cosine_with_restarts
 bash vessl-create.sh 8 0.0001 pretrain_hl polynomial
--- scripts/vessl-create.sh
+++ scripts/vessl-create.sh
@@ -21,7 +21,7 @@ vessl experiment create \
   --git-ref "/root/deardr:github/j6mes-lab/deardr/${GIT_REF}" \
   --dataset "/data/:kaist-jtlab/deardr-dataset" \
   --dataset "/cache/:kaist-jtlab/cache" \
-  -h reader=$READER_TRAIN -h learning_rate=$LEARNING_RATE -h batch_size=8 -h steps=1 -h eval_freq=15 -h lr_scheduler_type=${LR_SCHEDULER} \
-  -h val_reader=fever -h train_db=wiki-pretraining -h val_db=fever -h train_file=shuf_100k.jsonl -h val_file=shared_task_dev.jsonl \
+  -h reader=$READER_TRAIN -h learning_rate=$LEARNING_RATE -h batch_size=8 -h steps=1 -h eval_freq=75 -h lr_scheduler_type=${LR_SCHEDULER} \
+  -h val_reader=fever -h train_db=wiki-pretraining -h val_db=fever -h train_file=shuf_10k.jsonl -h val_file=shared_task_dev.jsonl \
   -h model_name=t5-base \
   -h DATA_ROOT=/data -h TRANSFORMERS_CACHE=/cache/transformers -h XDG_CACHE_HOME=/cache/pytorch
